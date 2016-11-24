f8ead6a93391991e12a59395783ca527c59052a4
Hans Trautlein <hans@trautlein.com>
2016-11-23 22:14:36 -0800

    Chakram tests written that test basic endpoints

--- predictIt.test.js
+++ predictIt.test.js
@@ -1,6 +1,29 @@
 const predictIt = require('./predictIt.js');
 
-test('Current version is 0.0.1', () => {
-  expect(predictIt.version).toBe('0.0.1');
+var chakram = require('chakram');
+var expect = chakram.expect;
+
+describe('PredictIt is up and responding to tests', () => {
+  it('should respond at the /all endpoint', () => {
+    return chakram.get('https://www.predictit.org/api/marketdata/all');
+  });
+
+  const category = '13';
+  const categoryName = 'US Politics';
+  it(`should respond at the /category endpoint for the ${categoryName} category`, () => {
+    return chakram.get(`https://www.predictit.org/api/marketdata/category/${category}`);
+  });
+
+  const group = '37';
+  const groupName = 'White House';
+  it(`should respond at the /group endpoint for the ${groupName} group`, () => {
+    return chakram.get(`https://www.predictit.org/api/marketdata/group/${group}`);
+  });
+
+  const ticker = 'CHINA.MANIPULATOR.063017';
+  it(`should respond at the /ticker endpoint for the ${ticker} market`, () => {
+    return chakram.get(`https://www.predictit.org/api/marketdata/ticker/${ticker}`);
+  });
+
 });
 
