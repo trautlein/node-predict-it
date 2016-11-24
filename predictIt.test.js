80bb2710427860a93b7a88a2e076ca36e818aa43
Hans Trautlein <hans@trautlein.com>
2016-11-23 20:59:24 -0800

    Write frist real test
    Test confirms that predictIt module has loaded correctly.

--- predictIt.test.js
+++ predictIt.test.js
@@ -1,4 +1,6 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+const predictIt = require('./predictIt.js');
+
+test('Current version is 0.0.1', () => {
+  expect(predictIt.version).toBe('0.0.1');
 });
 
