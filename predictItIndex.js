63a411b42168467f7462b759adfd10893fc4f2cd
Hans Trautlein <hans@trautlein.com>
2016-11-25 18:37:40 -0800

    Indexing file with numbers and categories/groups

---
+++ predictItIndex.js
@@ -0,0 +1,31 @@
+export default index = {
+  world: {
+    category: 4,
+
+    mideastAfrica: 49,
+    americas: 50,
+    asiaPacific: 51,
+    europe: 52,
+  },
+
+  usElections: {
+    category: 6,
+
+    stateLocal: 55,
+    national: 67,
+    electoralCollege: 75,
+    fundraising: 77,
+    congress: 82,
+  },
+
+  usPolitics: {
+    category: 13,
+
+    whiteHouse: 37,
+    congress: 38,
+    supremeCourt: 39,
+    otherEvents: 45,
+    cabinet: 81,
+  },
+}
+
