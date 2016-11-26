890761f07673c467f1048df2908850b36943c11c
Hans Trautlein <hans@trautlein.com>
2016-11-25 18:36:57 -0800

    Remove callbacks and library now uses promises

--- predictIt.js
+++ predictIt.js
@@ -1,121 +1,34 @@
 const request = require('request');
-
-const base = 'https://www.predictit.org/api/marketdata/';
+const rp      = require('request-promise');
+const Promise = require('bluebird');
 
 exports.version = '0.0.1';
 
-const ind = {
-  world: {
-    category: 4,
 
-    mideastAfrica: 49,
-    americas: 50,
-    asiaPacific: 51,
-    europe: 52,
-  },
+const base = 'https://www.predictit.org/api/marketdata/';
 
-  usElections: {
-    category: 6,
-
-    stateLocal: 55,
-    national: 67,
-    electoralCollege: 75,
-    fundraising: 77,
-    congress: 82,
-  },
-
-  usPolitics: {
-    category: 13,
-
-    whiteHouse: 37,
-    congress: 38,
-    supremeCourt: 39,
-    otherEvents: 45,
-    cabinet: 81,
-  },
+const apiCall = function(uri) {
+  return rp({uri, json: true})
 };
 
 
-exports.dry = function(url, callback) {
-
-  request(url, (err, response, body) => {
-    if (err) {
-      console.log("Error returning ${url} :", err);
-      callback(err, null);
-    } else {
-      console.log("It worked!");
-      callback(null, JSON.parse(body));
-    }
-  });
+exports.marketCall = function(market) {
+  const url = base + 'ticker/' + market;
+  return apiCall(url);
 };
 
-exports.market2 = function(ticker, callback) {
-  const url = base + 'ticker/' + ticker;
-
-  exports.dry(url, callback);
-};
-
-exports.market2('CHINA.MANIPULATOR.063017', (err, data) => {
-  console.log(data);
-});
-
-exports.market = function(ticker, callback) {
-  const url = base + 'ticker/' + ticker;
-
-  request(url, (err, response, body) => {
-    if (err) {
-      console.log("Error returning single market:", err);
-      callback(err, null);
-    } else {
-      console.log("It worked!");
-      callback(null, JSON.parse(body));
-    }
-  });
-};
-
-exports.group = function(group, callback) {
+exports.groupCall = function(group) {
   const url = base + 'group/' + group.toString();
-
-  request(url, (err, response, body) => {
-    if (err) {
-      console.log(`Error requesting ${group.toString()} group:`, err);
-      callback(err, null);
-    } else {
-      console.log("It worked!");
-      callback(null, JSON.parse(body));
-    }
-  });
+  return apiCall(url);
 };
 
-exports.category = function(category, callback) {
+exports.categoryCall = function(category) {
   const url = base + 'category/' + category.toString();
-
-  request(url, (err, response, body) => {
-    if (err) {
-      console.log(`Error returning ${category.toString()} category:`, err);
-      callback(err, null);
-    } else {
-      console.log("It worked!");
-      callback(null, JSON.parse(body));
-    }
-  });
+  return apiCall(url);
 };
 
-exports.all = function(callback) {
-  const url = base + 'all/';
-
-  request(url, (err, response, body) => {
-    if (err) {
-      console.log('Error returning all markets:', err);
-      callback(err, null);
-    } else {
-      console.log("It worked!");
-      callback(null, JSON.parse(body));
-    }
-  });
-}
-
-// exports.all((err, body) => {
-//   body.Markets.forEach(item => console.log(item.TickerSymbol, ':', item.Name));
-// });
+exports.allCall = function() {
+  const url = base + 'all';
+  return apiCall(url);
+};
 
