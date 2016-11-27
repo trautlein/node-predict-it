35e72c31fb96d0d89c0edabc102236712cdc6a1d
Hans Trautlein <hans@trautlein.com>
2016-11-26 18:33:04 -0800

    Add specific contract call and change returned data
    Now the calls that return multiple markets return an array
    of markets instead of returning an object with that array inside
    as the only real data inside. Makes the data easier to iterate over.

--- predictIt.js
+++ predictIt.js
@@ -1,13 +1,27 @@
-const rp = require('request-promise');
+const rp      = require('request-promise');
+const Promise = require('bluebird');
 
 exports.version = '0.0.2';
 
 const base = 'https://www.predictit.org/api/marketdata/';
 
 const apiCall = function(uri) {
-  return rp({uri, json: true})
+  return rp({uri, json: true});
 };
 
+
+exports.contractCall = function(targetContract) {
+  return new Promise(resolve => {
+    exports.marketCall(targetContract)
+      .then(market => {
+        market.Contracts.forEach((contract, index) => {
+          if (contract.TickerSymbol === targetContract) {
+            resolve(contract);
+          }
+        });
+      });
+  });
+};
 
 exports.marketCall = function(market) {
   const url = base + 'ticker/' + market;
@@ -16,16 +30,19 @@ exports.marketCall = function(market) {
 
 exports.groupCall = function(group) {
   const url = base + 'group/' + group.toString();
-  return apiCall(url);
+  return apiCall(url)
+    .then(d => d.Markets);
 };
 
 exports.categoryCall = function(category) {
   const url = base + 'category/' + category.toString();
-  return apiCall(url);
+  return apiCall(url)
+    .then(d => d.Markets);
 };
 
 exports.allCall = function() {
   const url = base + 'all';
-  return apiCall(url);
+  return apiCall(url)
+    .then(data => data.Markets);
 };
 
